4f0017359e4bee455a1bc88f9135ee46171e655d
smriti.mool <mool.smreeti@gmail.com>
2023-05-25 13:43:43 -0400

    add sign up user (backend) functionality

--- API/controllers/signupController.js
+++ API/controllers/signupController.js
@@ -1,4 +1,6 @@
-const User = require('../models/User');
+const User = require('../models/User.js');
+const UserType = require('../models/UserType.js');
+const CollegeInfo = require('../models/CollegeInfo.js');
 
 const signup = (req, res) => {
     let username = "";
@@ -18,7 +20,7 @@ const signup = (req, res) => {
 
 const signupUser = async (req, res) => {
     const {
-        collegInfoId,
+        collegeInfoId,
         firstName,
         lastName,
         email,
@@ -27,25 +29,40 @@ const signupUser = async (req, res) => {
         password
     } = req.body;
 
-
-    User.findOne({
-        email: email
-    })
-    .then((savedUser) => {
-        return res.status(422).json({ error: "user already exists" })
-    })
-
     try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(422).json({ error: "User already exists" });
+        }
 
-        await User.create(req.body);
-        res.json({message:"saved successfully"})
+        const collegeInfo = await CollegeInfo.findById(collegeInfoId);
+        if (!collegeInfo) {
+            return res.status(404).json({ error: "College info not found" });
+        }
+
+        const userType = await UserType.findOne({ code: "REGULAR_USER" });
+        if (!userType) {
+            return res.status(404).json({ error: "User type not found" });
+        }
+
+        await User.create({
+            collegeInfo,
+            userType,
+            firstName,
+            lastName,
+            email,
+            mobileNumber,
+            username,
+            password
+        });
+
+        return res.json({ message: "User saved successfully" });
         // return res.redirect('/login');
     } catch (error) {
-        const validationErrors = Object.values(error.errors).map(err => err.message);
+        const validationErrors = Object.values(error.errors).map((err) => err.message);
         req.flash('validationErrors', validationErrors);
         req.flash('data', req.body);
         return res.render('signup', {
-            passwordError: false,
             errors: req.flash('validationErrors')
         });
     }
