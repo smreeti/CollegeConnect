1492fb34e8af49086a731d208c19363a0210f83b
smriti.mool <mool.smreeti@gmail.com>
2023-06-03 21:31:32 -0400

    fix login page responsive design issue

--- UI/src/components/LoginComponent.jsx
+++ UI/src/components/LoginComponent.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { handleLoginFormValidation } from "../../utils/validation.js";
-import { API_TO_LOGIN_USER } from "../../utils/APIRequestUrl.js";
-import fetchData from "../../utils/FetchAPI.js";
+import { handleLoginFormValidation } from '../../utils/validation.js';
+import { API_TO_LOGIN_USER } from '../../utils/APIRequestUrl.js';
+import fetchData from '../../utils/FetchAPI.js';
 import { Link } from 'react-router-dom';
 
 const LoginComponent = () => {
@@ -15,22 +15,20 @@ const LoginComponent = () => {
   const handleOnChange = (event) => {
     const { name, value } = event.target;
 
-
     setUser((prevUser) => ({
       ...prevUser,
-      [name]: value
+      [name]: value,
     }));
   };
 
   const login = async (e) => {
-
-    console.log("Log");
+    console.log('Log');
     e.preventDefault();
 
     const form = document.forms.loginform;
     const user = {
       username: form.username.value,
-      password: form.password.value
+      password: form.password.value,
     };
 
     let formErrors = await handleLoginFormValidation(user);
@@ -43,20 +41,20 @@ const LoginComponent = () => {
 
   const loginUser = async (user) => {
     try {
-      const data = await fetchData(API_TO_LOGIN_USER, "POST", user);
+      const data = await fetchData(API_TO_LOGIN_USER, 'POST', user);
 
       console.log(data);
       if (!data.error) {
         const { token, user } = data.body;
-        localStorage.setItem("jwt", token)
-        localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem('jwt', token);
+        localStorage.setItem('user', JSON.stringify(user));
         navigate('/home');
       } else {
         setServerErrors(data.error);
         setFormErrors([]);
       }
     } catch (error) {
-      console.log("Error:", error);
+      console.log('Error:', error);
       setServerErrors(error);
     }
   };
@@ -66,61 +64,64 @@ const LoginComponent = () => {
   };
 
   return (
-    <>
-      <div id="login-container" className="container">
-        <div id='resrevtest' className="row  ">
-          <div className="col-lg-6 logostyle">
+    <div className="main-container">
+      <div id="main-login-container">
+        <div id="login-form-container">
+          <div className="login-details">
             <img className="login-logo" src="../../assets/logotestnew.png" />
-            <div className="login-form">
-              <div className="user-details">
-                <h2 className="color logfix">Login</h2>
-
-                <form className='formset' id="form1" name="loginform" method="POST" onSubmit={login} >
-                  <div className="form-group pad">
-                    <input id="username"
-                      name="username"
-                      placeholder="Enter mobile no., username, or email "
-                      value={user.username || ''}
-                      onChange={handleOnChange}
-                      key="UsernameInput"
-                      className="formheight" />
-                    <p className="required errormsg errpad1">{errors['username']}</p>
-                  </div>
-
-                  <div className="form-group secform pad">
-                    <input id="password"
-                      type='password'
-                      name="password"
-                      placeholder="Enter password"
-                      value={user.password || ''}
-                      onChange={handleOnChange}
-                      key="PasswordInput"
-                      className=" formheight formmar" />
-                    <p className="required errormsg errpad1">{errors['password']}</p>
-                  </div>
-
-                  <p>{serverErrors}</p>
-
-                  <p className="color forget">Forgot Password ? </p>
-                  {/* <label className="signin">
-                    <input type="checkbox" name="keep-signed-in" />
-                    Keep me signed in
-                  </label><br /> */}
-                  <button className="btn btn-primary btnblack" type="submit">Login</button>
-                  <h6 className="color">New User?
-                    <Link to="/signup"> Create an account</Link></h6>
-                </form>
-              </div>
-            </div>
+            <h2 className="color logfix">Login</h2>
           </div>
-          <div className="col-lg-6 bigimg p-0">
-            <div className="action-section banimg">
-              <img src="../../assets/testimg.jpg" alt="Image Placeholder" className="img-fluid banimg" />
+          <form
+            className="formset"
+            id="form1"
+            name="loginform"
+            method="POST"
+            onSubmit={login}
+          >
+            <div className="form-group pad">
+              <input
+                id="username"
+                name="username"
+                placeholder="Enter mobile no., username, or email "
+                value={user.username || ''}
+                onChange={handleOnChange}
+                key="UsernameInput"
+                className="formheight"
+              />
+              <p className="required errormsg errpad1">{errors['username']}</p>
             </div>
-          </div>
+
+            <div className="form-group secform pad">
+              <input
+                id="password"
+                type="password"
+                name="password"
+                placeholder="Enter password"
+                value={user.password || ''}
+                onChange={handleOnChange}
+                key="PasswordInput"
+                className=" formheight formmar"
+              />
+              <p className="required errormsg errpad1">{errors['password']}</p>
+            </div>
+
+            <p>{serverErrors}</p>
+
+            <p className="color forget">Forgot Password ? </p>
+            <button className="btn btn-primary btnblack" type="submit">
+              Login
+            </button>
+            <h6 className="color">
+              New User?
+              <Link to="/signup"> Create an account</Link>
+            </h6>
+          </form>
+        </div>
+        <div id="login-image">
+          <img src="../../assets/testimg.jpg" alt="Image Placeholder" />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
