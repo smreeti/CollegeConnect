225a03689ab4fa5456f5fa0acabe83825eb75038
cheskaitlin <kcheslie@gmail.com>
2023-08-07 12:58:54 -0400

    Added image source in ManageUsers component

--- UI/src/components/SchoolLinkComponent.jsx
+++ UI/src/components/SchoolLinkComponent.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const SchoolLink = ({ schoolName, onClick, to }) => {
+const SchoolLink = ({ schoolName, onClick, to, schoolImage }) => {
     return (
         <Link onClick={onClick} to={to}>
             <div className="card">
-                <img className="card-img-top" alt="" />
+                <img className="card-img-top" src={schoolImage} alt="" />
                 <div className="card-body">
                     <h1 className="card-title text-center">{schoolName}</h1>
                 </div>
