69162c07cd4c60b957d8607ba1d11bf36067bf7c
smriti.mool <mool.smreeti@gmail.com>
2023-08-07 16:58:34 -0400

    add be component

--- UI/utils/Auth.js
+++
@@ -1,4 +0,0 @@
-export const getLoggedInUser = () => {
-    const user = localStorage.getItem('user');
-    return user ? JSON.parse(user) : null;
-};
--- UI/utils/UserTypeConstants.js
+++
@@ -1,7 +0,0 @@
-const UserType = {
-  MASTER: "MASTER_ADMIN",
-  ADMIN: "SUPER_ADMIN",
-  REGULAR_USER: "REGULAR_USER",
-};
-
-export default UserType;
