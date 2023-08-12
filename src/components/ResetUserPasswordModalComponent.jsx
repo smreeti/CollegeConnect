4f699712cb9bfe0bb10adb730464a1249ad3238b
smriti.mool <mool.smreeti@gmail.com>
2023-08-12 10:39:58 -0400

    refactot reset modal

--- src/components/ResetUserPasswordModalComponent.jsx
+++ src/components/ResetUserPasswordModalComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import M from "materialize-css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes, faCheck } from "@fortawesome/free-solid-svg-icons";
