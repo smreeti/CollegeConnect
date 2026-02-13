690824fca6fb49bfd443efce3363deb58d3a445d
smriti.mool <mool.smreeti@gmail.com>
2026-02-13 17:34:08 -0500

    update webpack

--- UI/webpack.config.js
+++ UI/webpack.config.js
@@ -1,37 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-
-  mode: "production",
+  mode: "production", // production mode for smaller optimized build
   entry: "./src/app.jsx",
   output: {
-    filename: "[name].bundle.UIjs",
-    // path: path.resolve(__dirname, "public"),
-    path: path.resolve(__dirname, "build"), // use "build" folder
+    filename: "[name].bundle.js", // standard JS file
+    path: path.resolve(__dirname, "build"), // output folder for Vercel
+    publicPath: "/", // needed for React Router
   },
   module: {
     rules: [
       {
         test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              limit: 8192,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(html)$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: {
-              limit: 8192,
-            },
-          },
-        ],
+        use: [{ loader: "file-loader", options: { limit: 8192 } }],
       },
       {
         test: /\.css$/,
@@ -40,35 +22,16 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              [
-                "@babel/preset-env",
-                {
-                  targets: {
-                    ie: "11",
-                    edge: "15",
-                    safari: "10",
-                    firefox: "50",
-                    chrome: "49",
-                  },
-                },
-              ],
-              "@babel/preset-react",
-            ],
-          },
-        },
+        use: { loader: "babel-loader", options: { presets: ["@babel/preset-env", "@babel/preset-react"] } },
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // copy index.html to build/
+    }),
+  ],
   optimization: {
-    splitChunks: {
-      name: "vendor",
-      chunks: "all",
-    },
+    splitChunks: { name: "vendor", chunks: "all" },
   },
-
-
 };
